f36a963c390d8ad995722a169b766a89db36fc2f
Emil Johansson <emil.sweden@gmail.com>
2020-09-09 21:12:12 +0200

    Add Name field to keep track and some actuall legit youtube videos.

--- YogaVideo.cs
+++ YogaVideo.cs
@@ -11,6 +11,7 @@ namespace yoga_rest
     public class YogaVideo
     {
         public string Url { get; set;}
+        public string Name {get; set;}
         public VideoLengthInMinutes Minutes {get; set;}
     }
 
@@ -20,8 +21,19 @@ namespace yoga_rest
         {
             return new List<YogaVideo> {
                 new YogaVideo {
-                    Url = "url",
+                    Url ="https://www.youtube.com/embed/uN0VAViZ9nA",
+                    Name = "10 minute MOBILITY Yoga for Neck & Shoulder Relief | Sarah Beth Yoga",
                     Minutes = VideoLengthInMinutes.TenMinutes
+                },
+                new YogaVideo {
+                    Url = "https://www.youtube.com/embed/fBNJt2CbF5s",
+                    Name = "15 min Power Yoga Workout (level 1) BEACH BOD | Sarah Beth Yoga",
+                    Minutes = VideoLengthInMinutes.FifteenMinutes
+                },
+                new YogaVideo {
+                    Url = "https://www.youtube.com/embed/p1UxGHu9TVE",
+                    Name = "20 minute Power Yoga for Strength | Arm Balances: Crow & Hurdler's Pose",
+                    Minutes = VideoLengthInMinutes.TwentyMinutes
                 }
             };
         }
